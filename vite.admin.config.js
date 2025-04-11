863c6aa0b485d9225c5b5de202097af7a300360c
Daniel Arcé <daniel.arce@gmail.com>
2025-04-11 19:50:41 -0400

    whitespace

--- vite.admin.config.js
+++ vite.admin.config.js
@@ -3,17 +3,17 @@ import react from "@vitejs/plugin-react";
 import path from "path"
 
 export default {
-  plugins: [
-    v4wp({
-      input: "src/admin/main.jsx",
-      outDir: "assets/admin/dist",
-    }),
-    // wp_scripts(),
-    react(),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [
+        v4wp({
+            input: "src/admin/main.jsx",
+            outDir: "assets/admin/dist",
+        }),
+        // wp_scripts(),
+        react(),
+    ],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
 };
